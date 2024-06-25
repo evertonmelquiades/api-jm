ef9b99b76285d27163df8dce38373b0705614044
melquiades.everton <melquiades.everton@gmail.com>
2024-06-25 15:45:29 -0300

    dkbmfbl

--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,8 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::apiResource('users', UserController::class);
 Route::apiResource('employees', EmployeeController::class);
 Route::apiResource('businesses', BusinessController::class);
+
+require __DIR__.'/auth.php';
